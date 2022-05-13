435899168e05298b9e76e8006ace790ff934a115
C Bares <colinedwinbares@gmail.com>
2022-05-13 17:03:36 -0500

    touch events working, stop on mouseup/touchend now

--- test.js
+++ test.js
@@ -1,86 +1,84 @@
 const Tone = window.Tone;
 const buttonEl = document.querySelector('button');
+const headingEl = document.querySelector('h1');
+const paragraphEl = document.querySelector('paragraph');
 
 let osc;
 let playing = false;
 let started = false;
 
-// buttonEl.addEventListener('click', () => {
-//   console.log('button clicked');
-//   Tone.start()
-// })
-
-buttonEl.addEventListener('touchstart', () => {
-  console.log('button touched');
+buttonEl.addEventListener('click', () => {
+  console.log('button clicked');
   started = true;
-  osc = new Tone.Oscillator({
-    type: 'sine',
-    frequency: 440,
-    volume: -10,
-    partialCount: 0,
-  }).toDestination();
-  playing = true;
-
-});
-
-buttonEl.addEventListener('touchend', () => {
-  console.log('touch end fired')  
+  headingEl.innerText = 'Now press and hold to start the oscillator';
 })
 
-// window.addEventListener('click', (e) => {
-//   if (!playing) {
-//       clicked = true;
-//       playing = true;
-//       xVal = e.clientX;
-//       yVal = e.clientY;
+buttonEl.addEventListener('touchstart', () => {
+  if (!started) {
+    console.log('button touched');
+    buttonEl.innerText = 'Stop';
+    started = true;
+  }
+});
 
-//       console.log('x' + xVal, 'y' + yVal);
-//       osc = new Tone.Oscillator({
-//         type: 'sine',
-//         frequency: e.clientX + 30,
-//         volume: -15,
-//         partialCount: Math.floor(yVal / 100) + 2,
-//       }).toDestination();
-//       osc.start();
-//     } else {
-//       osc.stop();
-//       playing = false;
-//     }
-// });
+window.addEventListener('touchend', () => {
+  console.log('touch end fired on window')
+  if (playing) {
+    osc.stop();
+  }
+});
 
-// window.addEventListener('touchstart', (e) => {
-//   Tone.start();
-//   console.log('touch event', e.touches[0]);
-//   if (!playing) {
-//     clicked = true;
-//     playing = true;
-//     xVal = e.touches[0].clientX;
-//     yVal = e.touches[0].clientY;
+window.addEventListener('mouseup', () => {
+  console.log('mouse up fired on window');
+  if (playing) {
+    osc.stop();
+  }
+})
 
-//     console.log('x' + xVal, 'y' + yVal);
-//     osc = new Tone.Oscillator({
-//       type: 'sine',
-//       frequency: xVal,
-//       volume: -15,
-//       partialCount: Math.floor(yVal / 100) + 2,
-//     }).toDestination();
-//     osc.start();
-//   } else {
-//     osc.stop();
-//     playing = false;
-//   }
-// });
+window.addEventListener('mousedown', (e) => {
+  if (started) {
+      playing = true;
+      xVal = e.clientX;
+      yVal = e.clientY;
 
-// window.addEventListener('mousemove', (e) => {
-//   if (playing) {
-//     xVal = e.clientX;
-//     yVal = e.clientY;
-//     osc.set({
-//       frequency: xVal,
-//       partialCount: Math.floor(yVal / 50),
-//     });
-//   }
-// });
+      console.log('x' + xVal, 'y' + yVal);
+      osc = new Tone.Oscillator({
+        type: 'sine',
+        frequency: e.clientX + 30,
+        volume: -15,
+        partialCount: Math.floor(yVal / 100) + 2,
+      }).toDestination();
+      osc.start();
+    }
+});
+
+window.addEventListener('touchstart', (e) => {
+  if (started) {  
+      playing = true;
+      xVal = e.touches[0].clientX;
+      yVal = e.touches[0].clientY;
+  
+      console.log('x' + xVal, 'y' + yVal);
+      osc = new Tone.Oscillator({
+        type: 'sine',
+        frequency: xVal,
+        volume: -15,
+        partialCount: Math.floor(yVal / 100) + 2,
+      }).toDestination();
+      osc.start();
+  }
+});
+
+window.addEventListener('mousemove', (e) => {
+  if (playing) {
+    xVal = e.clientX;
+    yVal = e.clientY;
+    osc.set({
+      frequency: xVal,
+      partialCount: Math.floor(yVal / 50),
+    });
+  }
+});
 
 window.addEventListener('touchmove', (e) => {
   console.log(e.touches[0].clientX);
