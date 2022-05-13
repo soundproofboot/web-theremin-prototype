b5bafde888dfbc51df04751714e199427a22afcd
C Bares <colinedwinbares@gmail.com>
2022-05-13 16:21:23 -0500

    refactoring code for mobile touch events

--- test.js
+++ test.js
@@ -1,32 +1,92 @@
 const Tone = window.Tone;
+const buttonEl = document.querySelector('button');
 
 let osc;
 let playing = false;
-let clicked = false;
+let started = false;
 
-window.addEventListener('click', (e) => {
-  if (!clicked) {
-    clicked = true;
-    playing = true;
-    xVal = e.clientX;
-    yVal = e.clientY;
+// buttonEl.addEventListener('click', () => {
+//   console.log('button clicked');
+//   Tone.start()
+// })
 
-    console.log('x' + xVal, 'y' + yVal);
-    osc = new Tone.Oscillator({
-      type: 'sine',
-      frequency: e.clientX + 30,
-      volume: -15,
-      partialCount: Math.floor(yVal / 100) +2,
-    }).toDestination();
-    osc.start();
-  }
+buttonEl.addEventListener('touchstart', () => {
+  console.log('button touched');
+  started = true;
+  osc = new Tone.Oscillator({
+    type: 'sine',
+    frequency: 440,
+    volume: -10,
+    partialCount: 0,
+  }).toDestination();
+  playing = true;
+
 });
 
-window.addEventListener('mousemove', (e) => {
+buttonEl.addEventListener('touchend', () => {
+  console.log('touch end fired')  
+})
+
+// window.addEventListener('click', (e) => {
+//   if (!playing) {
+//       clicked = true;
+//       playing = true;
+//       xVal = e.clientX;
+//       yVal = e.clientY;
+
+//       console.log('x' + xVal, 'y' + yVal);
+//       osc = new Tone.Oscillator({
+//         type: 'sine',
+//         frequency: e.clientX + 30,
+//         volume: -15,
+//         partialCount: Math.floor(yVal / 100) + 2,
+//       }).toDestination();
+//       osc.start();
+//     } else {
+//       osc.stop();
+//       playing = false;
+//     }
+// });
+
+// window.addEventListener('touchstart', (e) => {
+//   Tone.start();
+//   console.log('touch event', e.touches[0]);
+//   if (!playing) {
+//     clicked = true;
+//     playing = true;
+//     xVal = e.touches[0].clientX;
+//     yVal = e.touches[0].clientY;
+
+//     console.log('x' + xVal, 'y' + yVal);
+//     osc = new Tone.Oscillator({
+//       type: 'sine',
+//       frequency: xVal,
+//       volume: -15,
+//       partialCount: Math.floor(yVal / 100) + 2,
+//     }).toDestination();
+//     osc.start();
+//   } else {
+//     osc.stop();
+//     playing = false;
+//   }
+// });
+
+// window.addEventListener('mousemove', (e) => {
+//   if (playing) {
+//     xVal = e.clientX;
+//     yVal = e.clientY;
+//     osc.set({
+//       frequency: xVal,
+//       partialCount: Math.floor(yVal / 50),
+//     });
+//   }
+// });
+
+window.addEventListener('touchmove', (e) => {
+  console.log(e.touches[0].clientX);
   if (playing) {
-    xVal = e.clientX;
-    yVal = e.clientY;
-    console.log('x' + xVal, 'y' + yVal);
+    xVal = e.touches[0].clientX;
+    yVal = e.touches[0].clientY;
     osc.set({
       frequency: xVal,
       partialCount: Math.floor(yVal / 50),
@@ -45,7 +105,7 @@ window.addEventListener('mousemove', (e) => {
 //   })
 // });
 
-const buttonEl = document.querySelector('button');
+// const buttonEl = document.querySelector('button');
 // const volumeEl = document.querySelector('#volume');
 // const frequencyEl = document.querySelector('#frequency');
 
